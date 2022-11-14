3da82a989f51c35abe99cdd36e7efab81b73dc43
Vinícius Maximiliano <vinnemaax@gmail.com>
2022-11-13 23:16:07 -0300

    Front do mapa finalizado

--- client/src/components/mapOptions/MapOptions.jsx
+++ client/src/components/mapOptions/MapOptions.jsx
@@ -3,6 +3,14 @@ import './mapOptions.css'
 
 export default function MapOptions() {
   return (
-    <div className='mapOptions'>MapOptions</div>
+    <div className='mapOptions'>
+      <div className="optionsContainer">
+          <img alt='' title='Plastico' className='containerItem' src='https://cdn-icons-png.flaticon.com/512/1039/1039778.png'></img>
+          <img alt='' title='Papel' className='containerItem' src='https://cdn-icons-png.flaticon.com/512/1039/1039780.png'></img>
+          <img alt='' title='Vidro' className='containerItem' src='https://cdn-icons-png.flaticon.com/512/1039/1039781.png'></img>
+          <img alt='' title='Organico' className='containerItem' src='https://cdn-icons-png.flaticon.com/512/1039/1039775.png'></img>
+          <img alt='' title='Metal' className='containerItem' src='https://cdn-icons-png.flaticon.com/512/1039/1039782.png'></img>
+      </div>
+    </div>
   )
 }
